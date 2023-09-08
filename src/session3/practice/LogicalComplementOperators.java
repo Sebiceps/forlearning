a0cb31e2d8164d2724cf990aa1fe531f63aafb2e
Sebastian <seb.conete2001@gmail.com>
2023-09-08 21:20:03 +0300

    Session3

---
+++ src/session3/practice/LogicalComplementOperators.java
@@ -0,0 +1,12 @@
+package session3.practice;
+
+public class LogicalComplementOperators {
+
+    public static void main(String[] args) {
+        boolean isFirstTime = true;
+        boolean isRegistered = false;
+
+        System.out.println(!isFirstTime);
+        System.out.println(!isRegistered);
+    }
+}
---
+++ src/session3/practice/UnaryOperators.java
@@ -0,0 +1,18 @@
+package session3.practice;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+        int firstNumber = +10;
+        int secondNumber = -20;
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+
+        double myDouble = 23.5;
+        int myInt = 22;
+
+        System.out.println(myDouble > myInt);
+        System.out.println(myDouble < myInt);
+    }
+}
